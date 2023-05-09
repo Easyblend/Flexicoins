5ac26e8a455201595a68091962b97d699b0323f8
easyblend <kenzieemma072@gmail.com>
2023-05-09 00:26:29 +0000

    upgraded payment functionality

--- src/views/examples/Modal.jsx
+++ src/views/examples/Modal.jsx
@@ -66,7 +66,7 @@ const Modal = ({
         const paystack = new PaystackPop();
 
         paystack.newTransaction({
-          key: process.env.REACT_APP_PAYSTACK_KEY,
+          key: "pk_test_477bc5aa719010f8bc1b1f78af44b9e4da32c035",
           email: email,
           amount: purchasingAmount * 100,
           onSuccess: async () => {
